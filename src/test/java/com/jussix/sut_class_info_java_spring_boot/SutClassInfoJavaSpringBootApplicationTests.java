456ffc82f87b478ad83df6b38f97cf496cd466b8
JUSSIXPC\Patch <Patcharapolwor28112544@gmail.com>
2025-06-04 17:00:19 +0700

    first commit : initial project

---
+++ src/test/java/com/jussix/sut_class_info_java_spring_boot/SutClassInfoJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jussix.sut_class_info_java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SutClassInfoJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
